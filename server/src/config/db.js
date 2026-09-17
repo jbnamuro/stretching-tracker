853da5e57a406cffdbac744675d68c7d2ab0f290
jbnamuro <jbnamuro12@gmail.com>
2026-09-17 18:39:16 -0400

    prisma update fix

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,17 @@
+import "dotenv/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
 import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+
+const adapter = new PrismaNeon({
+    connectionString: process.env.DATABASE_URL
+});
 
 const prisma = new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"]
 });
 
